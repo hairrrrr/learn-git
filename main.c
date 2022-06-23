baa4fcb0ca50e8b1fb2747b5a253abe64677b870
hairrrrr <shepardwang2020@outlook.com>
2022-06-23 12:34:37 +0800

    fix bug

--- main.c
+++ main.c
@@ -5,17 +5,13 @@ int main()
 
 	printf("hello world ~~~");
 
-	for(int i = 0; i < 5; ++i)
-	{
-		printf("hello world");
+	printf("hello world");
 
+	// 新功能
+	puts("program ends");
+	putchar('x');
 
-		// 新功能
-		puts("program ends");
-		putchar('x');
-
-		// 这里继续开发新功能
-	}
+	// 这里继续开发新功能
 
 	return 0;
 }
